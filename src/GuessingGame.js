f1972393fa0ea933d1fdb0728a057461861f742b
berger <robertberger.dev@gmail.com>
2023-02-23 19:39:20 -0500

    Got the main message to render on the screen

--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function GuessingGame (
-    
-)
+function GuessingGame() {
+    return (
+        <div>
+            <h2>I am thinking of a number between 1 and 100. Guess the Lucky Number!</h2>
+        </div>
+    )
+}
 
 export default GuessingGame
