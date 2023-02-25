ec2f287e866536081443ae2d35f5c26cb5c79bab
berger <robertberger.dev@gmail.com>
2023-02-25 09:10:39 -0500

    created RandomNumber function within RandomNumber component

--- src/RandomNumber.js
+++ src/RandomNumber.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 function RandomNumber() {
     const [ randomNumber, setRandomNumber ] = useState('null');
+
+    function makeRandomNum() {
+        let randomNum = Math.floor(Math.random() * 100);
+        localStorage.setItem("randomNum", JSON.stringify(randomNum));
+        return randomNum
+    }
 }
 
 export default RandomNumber
