e1a6543644931bef09966341b1c790cce55672e2
berger <robertberger.dev@gmail.com>
2023-02-25 14:06:44 -0500

    moved RandomNumber to GuessingGame.js as per instructions

--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import RandomNumber from './RandomNumber';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function GuessingGame() {
+    const [guessAttempts, saveGuessAttempts] = useState(null);
+    const [userGuess, saveUserGuess] = useState("");
 
+    const RandomNumber = () => {
+        const [ randomNumber, saveRandomNumber ] = useState(null);
+    
+        useEffect(() => {
+            if (randomNumber === null) {
+                saveRandomNumber(
+                    JSON.parse(localStorage.getItem("randomNum")) || makeRandomNum()
+                )
+            }
+        }, []);
+    
+        function makeRandomNum() {
+            let randomNum = Math.floor(Math.random() * 100);
+            localStorage.setItem("randomNum", JSON.stringify(randomNum));
+            return randomNum
+        }
+        return <p>{randomNumber}</p>
+        
+    }
+
+    useEffect(() => {
+        if (guessAttempts === null) {
+            saveGuessAttempts(
+                JSON.parse(localStorage.getItem("numOfGuesses")) || 0
+            )
+        }
+    }, []);
+
+    function makeTypingWork(event) {
+        saveUserGuess(event.target.value)
+    }
+
+    function buttonClickEvent(event) {
+            event.preventDefault();
+
+    }
 
     return (
         <div>
             <h2>I am thinking of a number between 1 and 100. Guess the Lucky Number!</h2>
-            <RandomNumber />
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Control type="text" value={userGuess} placeholder="Enter Your Guess Here" onChange={makeTypingWork}/>
+                    <Form.Label>You have made {guessAttempts} attempts.</Form.Label>
+                    <Button type="submit">Guess</Button>
+                </Form.Group>
+            </Form>
         </div>
     )
 }
--- src/RandomNumber.js
+++ src/RandomNumber.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const RandomNumber = () => {
-    const [ randomNumber, saveRandomNumber ] = useState(null);
+// const RandomNumber = () => {
+//     const [ randomNumber, saveRandomNumber ] = useState(null);
 
-    useEffect(() => {
-        if (randomNumber === null) {
-            saveRandomNumber(
-                JSON.parse(localStorage.getItem("randomNum")) || makeRandomNum()
-            )
-        }
-    }, []);
+//     useEffect(() => {
+//         if (randomNumber === null) {
+//             saveRandomNumber(
+//                 JSON.parse(localStorage.getItem("randomNum")) || makeRandomNum()
+//             )
+//         }
+//     }, []);
 
-    function makeRandomNum() {
-        let randomNum = Math.floor(Math.random() * 100);
-        localStorage.setItem("randomNum", JSON.stringify(randomNum));
-        return randomNum
-    }
-    return <p>{randomNumber}</p>
+//     function makeRandomNum() {
+//         let randomNum = Math.floor(Math.random() * 100);
+//         localStorage.setItem("randomNum", JSON.stringify(randomNum));
+//         return randomNum
+//     }
+//     return <p>{randomNumber}</p>
     
-}
+// }
 
-export default RandomNumber
+// export default RandomNumber
